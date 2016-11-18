e0a42290e0a99b125ebbec791754bbaba9970dde
ompilompier <ompilompier@gmail.com>
2016-11-18 17:02:31 +0100

    indentation

--- SocsZombies/packageSOCS/Agent.py
+++ SocsZombies/packageSOCS/Agent.py
@@ -8,7 +8,7 @@ class Agent:
 		self.yPos = yPos
 		self.health = health
 		self.stateOfBeing = 0 #0 for human 1 for zombie
-    #
+	#
 	
 	def Move(self, grid):
         # change position
@@ -16,12 +16,12 @@ class Agent:
 		newPosition = []
 		tmpReturn = [oldPosition, newPosition]
 		return tmpReturn
-    #    
+	#    
 
 	def ChangeHealth(self, amountHealthAlteration):
 		self.health = self.health + amountHealthAlteration
 		return self.health
-    #
+	#
         
 	def GetState(self):
 		state = self.stateOfBeing

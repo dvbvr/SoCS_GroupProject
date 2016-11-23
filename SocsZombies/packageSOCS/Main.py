fc7019df7a2a5fa642caa5a7932a091ec72547a2
ompilompier <ompilompier@gmail.com>
2016-11-23 18:55:49 +0100

    apperently i suck at testing... but now it works for me!
    now it should work, and its actually a 5 by 5 field and 20 time-steps
    read previous commit for more information

--- SocsZombies/packageSOCS/Main.py
+++ SocsZombies/packageSOCS/Main.py
@@ -7,34 +7,34 @@ nbrAgents = 10
 nbrHuman = 10
 nbrDead = 0
 nbrZombies = nbrAgents-nbrHuman
-fieldSize = 3
+fieldSize = 5
 Agents = []
 
 fieldInfo = np.zeros((fieldSize,fieldSize), dtype=object)
-for it in range(5):
-	for i in range(3):
-		for j in range(3):
-			fieldInfo[i][j] = [0,0,0]
-	#
-	
-	#initializes the humans on the fields
-	for i in range(0,nbrHuman):
-		xPos = np.random.random_integers(0,fieldSize-1)
-		yPos = np.random.random_integers(0,fieldSize-1)
-		Agents.append(HumanRandom(xPos, yPos, 10))
-		fieldInfo[xPos][yPos][2] = fieldInfo[xPos][yPos][2] + 1 #one human added
-		fieldInfo[xPos][yPos].append(i)
-	#
-	
-	#initializes the zombies on the fields
-	for i in range(nbrHuman,nbrAgents):
-		xPos = np.random.random_integers(0,fieldSize-1)
-		yPos = np.random.random_integers(0,fieldSize-1)
-		#Agents.append(ZombieRandom(xPos, yPos, 10))
-		fieldInfo[xPos][yPos][1] = fieldInfo[xPos][yPos][1] + 1 #one zombie added
-		fieldInfo[xPos][yPos].append(i)
-	#
-	
+
+for i in range(fieldSize):
+	for j in range(fieldSize):
+		fieldInfo[i][j] = [0,0,0]
+#
+
+#initializes the humans on the fields
+for i in range(0,nbrHuman):
+	xPos = np.random.random_integers(0,fieldSize-1)
+	yPos = np.random.random_integers(0,fieldSize-1)
+	Agents.append(HumanRandom(xPos, yPos, 10))
+	fieldInfo[xPos][yPos][2] = fieldInfo[xPos][yPos][2] + 1 #one human added
+	fieldInfo[xPos][yPos].append(i)
+#
+
+#initializes the zombies on the fields
+for i in range(nbrHuman,nbrAgents):
+	xPos = np.random.random_integers(0,fieldSize-1)
+	yPos = np.random.random_integers(0,fieldSize-1)
+	#Agents.append(ZombieRandom(xPos, yPos, 10))
+	fieldInfo[xPos][yPos][1] = fieldInfo[xPos][yPos][1] + 1 #one zombie added
+	fieldInfo[xPos][yPos].append(i)
+#
+for it in range(20):
 	#moves all agents once
 	for i in range(0,nbrAgents):
 		[pos1,pos2] = Agents[i].Move(fieldInfo)
@@ -50,7 +50,7 @@ for it in range(5):
 			fieldInfo[pos1[0]][pos1[1]][1] = fieldInfo[pos1[0]][pos1[1]][1] - 1 #one zombie removed
 			fieldInfo[pos2[0]][pos2[1]][1] = fieldInfo[pos2[0]][pos2[1]][1] + 1 #one zombie added
 		fieldInfo[pos2[0]][pos2[1]].append(i)	
-	#
-	#Plot.AllFields(Plot ,fieldInfo , 3 , 3) 
+	#Plot.AllFields(Plot ,fieldInfo , 5 , 5) 
 	#The above line does one print and is probably the wrong way of using the plot code xD
 	#Also the import made on line 4 might be just as bad
+#
